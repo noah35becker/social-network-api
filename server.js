59f780666e0704433b15b67b254b071c2ee99019
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-10-01 23:33:23 -0400

    set up server + mongoose/mongodb connection

---
+++ server.js
@@ -0,0 +1,29 @@
+
+// IMPORTS
+const express = require('express');
+    const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+
+
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// app.use(require('./api-routes'));
+
+
+
+// INITIALIZE
+
+// Connect Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('debug', true); // This logs all Mongo queries that are executed
+
+
+// Connect server
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
